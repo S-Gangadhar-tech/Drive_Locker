4a8e151adfcb0248c356d6c5e64b8f053760172b
gangadhar <sgangadhar.tech@gmail.com>
2026-06-16 15:08:47 +0530

    fix: run project issues

--- Server/src/main/java/com/drivelocker/DriveLocker/DriveLockerApplication.java
+++ Server/src/main/java/com/drivelocker/DriveLocker/DriveLockerApplication.java
@@ -1,19 +1,19 @@
 package com.drivelocker.DriveLocker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
 @SpringBootApplication(scanBasePackages = "com.drivelocker.DriveLocker")
 @EntityScan(basePackages = "com.drivelocker.DriveLocker.models")
 public class DriveLockerApplication {
 
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(DriveLockerApplication.class, args);
-//		System.out.println(cloudinaryUrl);
 	}
 
 }
